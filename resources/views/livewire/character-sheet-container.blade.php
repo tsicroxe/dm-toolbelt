811eb2df74b8342d9281b2c1d417524d0caa4f5a
tsicroxE <arbanakus@gmail.com>
2022-04-23 15:28:06 -0700

    updated selecting character

--- resources/views/livewire/character-sheet-container.blade.php
+++ resources/views/livewire/character-sheet-container.blade.php
@@ -1,6 +1,6 @@
 <div class="flex flex-col justify-center min-h-screen py-12 bg-gray-50 sm:px-6 lg:px-8">
     <div class="ml-16 flex flex-col items-center justify-start">
-    <p class="self-start ml-12 text-3xl text-sky-400"> Your characters</p>
+    <p class="self-start ml-12 text-3xl text-blue-400"> Your characters</p>
 
             <table class="table-fixed border-double border-4 border-sky-500 m-5 text-left">
                 <thead>
@@ -13,9 +13,9 @@
 
                     @foreach($characters as $character)
                     <tr class="h-16">
-                        <td>
-                            {{$character->name}}
-                        </td>
+                            <td onclick="window.location='/characters/{{$character->id}}'" class="hover:bg-green-300">
+                                {{$character->name}}
+                            </td>
                         <td>
                         <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 mr-5 rounded" wire:click="delete({{$character->id}})">
                                 Delete
