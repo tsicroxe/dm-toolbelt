2871d21a4fcddc6bac3304da5d8d6dd833f649e2
tsicroxE <arbanakus@gmail.com>
2022-04-24 15:07:25 -0700

    WIP

--- app/Http/Livewire/TokenizerContainer.php
+++ app/Http/Livewire/TokenizerContainer.php
@@ -3,9 +3,22 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class TokenizerContainer extends Component
 {
+    use WithFileUploads;
+
+    public $photo;
+
+    public function upload()
+    {
+        $this->validate([
+            'photo' => 'image|max:1024', // 1MB Max
+        ]);
+        $this->photo->store('photos');
+    }
+
     public function render()
     {
         return view('livewire.tokenizer-container');
--- resources/views/livewire/tokenizer-container.blade.php
+++ resources/views/livewire/tokenizer-container.blade.php
@@ -3,12 +3,26 @@
         <div class="ml-16">
             <div class="flex mb-4">
                 <div class="flex w-4/6 bg-gray-500 h-screen">
-
+ 
                 </div>
                 <div class="flex flex-col p-5 items-center w-2/6 bg-gray-400 h-screen">
-                    <div class="bg-white box-content h-16 w-16 md:h-32 md:w-32 lg:h-64 lg:w-64 p-4 border-4 ...">
-                    <!-- ... -->
+                    <div class="bg-transparent box-content h-16 w-16 md:h-32 md:w-32 lg:h-64 lg:w-64 p-4 border-4" >
+                    <!-- White box -->
+                    <div id="html-content-holder">
+                        <img class="absolute h-16 w-16 md:h-32 md:w-32 lg:h-64 lg:w-64" src="{{ asset('photos/default_frame.png') }}">
+
+                        @if ($photo)
+                        <img class="rounded-full" src="{{ $photo->temporaryUrl() }}">
+                        @endif
+                        </div>
                     </div>
+                    <form wire:submit.prevent="upload">
+                        <input type="file" wire:model="photo">
+                            @error('photo') <span class="error">{{ $message }}</span> @enderror
+                        <button type="submit">Save Photo</button>
+                        
+                        <a id="btn-Convert-Html2Image">convertto image</a>
+                        </form>
                 </div>
             </div>
         </div>
