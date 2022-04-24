1de1ef1a92ed2a1d0af5a954fdc6400fe9749840
tsicroxE <arbanakus@gmail.com>
2022-04-24 13:16:15 -0700

    start of tokenizer layout

--- resources/views/livewire/tokenizer-container.blade.php
+++ resources/views/livewire/tokenizer-container.blade.php
@@ -1,7 +1,15 @@
 <div class="flex flex-col justify-center min-h-screen py-12 bg-gray-50 sm:px-6 lg:px-8">
 
-    <div class="ml-16 flex items-center justify-center">
-        
-        <p>Tokenizer container</p>
-    </div>
+        <div class="ml-16">
+            <div class="flex mb-4">
+                <div class="flex w-4/6 bg-gray-500 h-screen">
+
+                </div>
+                <div class="flex flex-col p-5 items-center w-2/6 bg-gray-400 h-screen">
+                    <div class="bg-white box-content h-16 w-16 md:h-32 md:w-32 lg:h-64 lg:w-64 p-4 border-4 ...">
+                    <!-- ... -->
+                    </div>
+                </div>
+            </div>
+        </div>
 </div>  
