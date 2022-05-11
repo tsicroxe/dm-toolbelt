6ae7aa49683f0537a2fdb5fbb8dfe698008282f6
tsicroxE <arbanakus@gmail.com>
2022-05-11 11:24:37 -0700

    moooooore tests

--- tests/Feature/Livewire/CharacterViewerTest.php
+++ tests/Feature/Livewire/CharacterViewerTest.php
@@ -26,79 +26,145 @@ class CharacterViewerTest extends TestCase
         $component->assertStatus(200);
     }
 
-        /** @test */
-        public function can_add_equipment()
-        {
-            $user = User::factory()->create();
-            $this->actingAs($user);
-            $character = Character::factory()->create(['user_id' => $user->id]);
-            $equipment = Equipment::factory()->create();
-            Livewire::test(CharacterViewer::class, ['character' => $character])
-                ->set('itemForm.id', $equipment->id)
-                ->set('itemForm.quantity', 5)
-                ->call('addItemAndQuantity', ['id' => $equipment->id, 'name'])
-                ->assertSee($equipment->name)
-                ->assertSee(5);
-        }
+    /** @test */
+    public function can_add_equipment()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+        $character = Character::factory()->create(['user_id' => $user->id]);
+        $equipment = Equipment::factory()->create();
+        Livewire::test(CharacterViewer::class, ['character' => $character])
+            ->set('itemForm.id', $equipment->id)
+            ->set('itemForm.quantity', 5)
+            ->call('addItemAndQuantity', ['id' => $equipment->id, 'name'])
+            ->assertSee($equipment->name)
+            ->assertSee(5);
+    }
 
-        /** @test */
-        public function can_delete_equipment()
-        {
-            $user = User::factory()->create();
-            $this->actingAs($user);
-            $character = Character::factory()->create(['user_id' => $user->id]);
-            $equipment = Equipment::factory()->create();
-            CharacterEquipment::factory()->create([
-                'character_id' => $character->id,
-                'equipment_id' => $equipment->id
-            ]);
+    /** @test */
+    public function can_delete_equipment()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+        $character = Character::factory()->create(['user_id' => $user->id]);
+        $equipment = Equipment::factory()->create();
+        CharacterEquipment::factory()->create([
+            'character_id' => $character->id,
+            'equipment_id' => $equipment->id
+        ]);
 
-            $this->assertDatabaseCount('character_equipment', 1);
+        $this->assertDatabaseCount('character_equipment', 1);
 
-            Livewire::test(CharacterViewer::class, ['character' => $character])
-                ->set('itemForm.id', $equipment->id)
-                ->set('itemForm.quantity', 5)
-                ->call('addItemAndQuantity', ['id' => $equipment->id, 'name'])
-                ->call('deleteItem', $equipment->id);
+        Livewire::test(CharacterViewer::class, ['character' => $character])
+            ->set('itemForm.id', $equipment->id)
+            ->set('itemForm.quantity', 5)
+            ->call('addItemAndQuantity', ['id' => $equipment->id, 'name'])
+            ->call('deleteItem', $equipment->id);
 
-            $this->assertDatabaseCount('character_equipment', 0);
+        $this->assertDatabaseCount('character_equipment', 0);
+    }
 
-        }
+    /** @test */
+    public function can_add_spells()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+        $character = Character::factory()->create(['user_id' => $user->id]);
+        $spell = Spell::factory()->create();
+        Livewire::test(CharacterViewer::class, ['character' => $character])
+            ->set('spellForm.id', $spell->id)
+            ->call('addSpell')
+            ->assertSee($spell->name);
+    }
 
-        /** @test */
-        public function can_add_spells()
-        {
-            $user = User::factory()->create();
-            $this->actingAs($user);
-            $character = Character::factory()->create(['user_id' => $user->id]);
-            $spell = Spell::factory()->create();
-            Livewire::test(CharacterViewer::class, ['character' => $character])
-                ->set('spellForm.id', $spell->id)
-                ->call('addSpell')
-                ->assertSee($spell->name);
-        }
+    /** @test */
+    public function can_delete_spells()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+        $character = Character::factory()->create(['user_id' => $user->id]);
+        $spell = Spell::factory()->create();
+        CharacterSpell::factory()->create([
+            'character_id' => $character->id,
+            'spell_id' => $spell->id
+        ]);
 
-                /** @test */
-        public function can_delete_spells()
-        {
-            $user = User::factory()->create();
-            $this->actingAs($user);
-            $character = Character::factory()->create(['user_id' => $user->id]);
-            $spell = Spell::factory()->create();
-            CharacterSpell::factory()->create([
-                'character_id' => $character->id,
-                'spell_id' => $spell->id
-            ]);
+        $this->assertDatabaseCount('character_spell', 1);
 
-            $this->assertDatabaseCount('character_spell', 1);
+        Livewire::test(CharacterViewer::class, ['character' => $character])
+            ->set('spellForm.id', $spell->id)
+            ->call('addSpell')
+            ->call('deleteSpell', $spell->id);
 
-            Livewire::test(CharacterViewer::class, ['character' => $character])
-                ->set('spellForm.id', $spell->id)
-                ->call('addSpell')
-                ->call('deleteSpell', $spell->id);
+        $this->assertDatabaseCount('character_spell', 0);
+    }
 
-            $this->assertDatabaseCount('character_spell', 0);
+    /** @test */
+    public function setting_str_score_adjusts_modifier()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+        $character = Character::factory()->create(['user_id' => $user->id, 'str_score' => 20]);
 
-        }
+        Livewire::test(CharacterViewer::class, ['character' => $character])
+            ->assertViewHas('str_mod', 5);
+    }
 
+    /** @test */
+    public function setting_dex_score_adjusts_modifier()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+        $character = Character::factory()->create(['user_id' => $user->id, 'dex_score' => 18]);
+
+        Livewire::test(CharacterViewer::class, ['character' => $character])
+            ->assertViewHas('dex_mod', 4);
+    }
+
+    /** @test */
+    public function setting_con_score_adjusts_modifier()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+        $character = Character::factory()->create(['user_id' => $user->id, 'con_score' => 15]);
+
+        Livewire::test(CharacterViewer::class, ['character' => $character])
+            ->assertViewHas('con_mod', 2);
+    }
+
+
+    /** @test */
+    public function setting_int_score_adjusts_modifier()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+        $character = Character::factory()->create(['user_id' => $user->id, 'int_score' => 16]);
+
+        Livewire::test(CharacterViewer::class, ['character' => $character])
+            ->assertViewHas('int_mod', 3);
+    }
+
+
+    /** @test */
+    public function setting_wis_score_adjusts_modifier()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+        $character = Character::factory()->create(['user_id' => $user->id, 'wis_score' => 6]);
+
+        Livewire::test(CharacterViewer::class, ['character' => $character])
+            ->assertViewHas('wis_mod', -2);
+    }
+
+
+    /** @test */
+    public function setting_cha_score_adjusts_modifier()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+        $character = Character::factory()->create(['user_id' => $user->id, 'cha_score' => 10]);
+
+        Livewire::test(CharacterViewer::class, ['character' => $character])
+            ->assertViewHas('cha_mod', 0);
+    }
 }
